83d3bfb0b31cc5aceb17e04111f5f1291fa4649b
Stefan Fiedler <sfi@stefans-air.speedport.ip>
2018-12-08 23:27:06 +0100

    Docs and update * added inline docs * added update function to TodoController

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -5,13 +5,29 @@ namespace App\Http\Controllers;
 use App\Todo;
 use Illuminate\Http\Request;
 
+/**
+ * This controller handles all task related http requests.
+ *
+ * @package App\Http\Controllers
+ */
 class TodoController extends Controller
 {
+    /**
+     * Return all existing tasks.
+     *
+     * @return Todo[]|\Illuminate\Database\Eloquent\Collection
+     */
     public function findAll()
     {
         return Todo::all();
     }
 
+    /**
+     * Persist new task from given request object.
+     *
+     * @param Request $request
+     * @return Todo
+     */
     public function store(Request $request)
     {
         $todo = new Todo;
@@ -23,14 +39,39 @@ class TodoController extends Controller
         return $todo;
     }
 
+    /**
+     * Remove existing task with given id from request.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function remove(Request $request)
     {
         $todo = Todo::find($request->id);
 
-        if($todo) {
+        if ($todo) {
             $todo->delete();
 
             return response()->json(array('success' => true), 200);
+        }
+
+        return response()->json(array('success' => false), 400);
+    }
+
+    /**
+     * Update given request from request.
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request)
+    {
+        $todo = Todo::find($request->id);
+
+        if ($todo) {
+            $todo->title = $request->title;
+
+            return $todo->save();
         }
 
         return response()->json(array('success' => false), 400);
